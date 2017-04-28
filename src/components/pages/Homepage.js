efde12ca88688c63e8b3eed38d31848111b24a80
Anil Kulkarni <anil.kulkarni@outlook.com>
2017-04-28 16:20:46 -0700

    Stub out the homepage component

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -1,12 +1,16 @@
 import React, { PureComponent } from 'react';
+import Markdown from 'react-markdown';
 
 import historyProps from '../types/history';
+
+const homepageContent = `# DBC Skills
+Content goes here`;
 
 class Homepage extends PureComponent {
   render() {
     return (
       <div>
-        <p>This is the homepage</p>
+        <Markdown source={homepageContent} />
       </div>
     );
   }
